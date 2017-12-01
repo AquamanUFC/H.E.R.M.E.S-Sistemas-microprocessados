2ec24d6d45b150a3854e578b6fcd5cfdcdc1a8c8
Ícaro <icaroceix@gmail.com>
2017-12-01 15:42:26 -0200

    Tela inicial pronta

--- src/android/EPC/app/src/main/java/microprocessados/ufc/epc/MainScreenActivity.java
+++ src/android/EPC/app/src/main/java/microprocessados/ufc/epc/MainScreenActivity.java
@@ -12,24 +12,6 @@ import android.view.MenuItem;
 public class MainScreenActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        new BluetoothManager(this);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
